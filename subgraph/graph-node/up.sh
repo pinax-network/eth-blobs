17c07e389b51598ef2005253963f74a31f140aa2
YaroShkvorets <shkvorets@gmail.com>
2024-03-02 18:17:58 -0500

    drop initialBlock

--- subgraph/graph-node/up.sh
+++ subgraph/graph-node/up.sh
@@ -31,7 +31,7 @@ main() {
 
 prepare() {
   if [[ -z "$SUBSTREAMS_ENDPOINT" ]]; then
-    export SUBSTREAMS_ENDPOINT="https://goerli-cl.substreams.pinax.network:443"
+    export SUBSTREAMS_ENDPOINT="https://holesky-cl.substreams.pinax.network:443"
   fi
   if [[ -z "$SUBSTREAMS_API_TOKEN" ]]; then
     echo "Your environment is not corrrectly configured to launch Docker Compose configuration."
